08a08dcc3cbd62d31bd8d91633ebfbb89f0dd6ab
Brandon Johnson <brj.webside@gmail.com>
2021-08-09 22:38:24 -0500

    Creating the initial scaffolding for the website. Includes a uston hook for using axios to communicate with the backend, subject to change later

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import useAxios from "./axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// NOTE: will start with only one component. simple web app no need to overcomplicate it.
+
+// 
+const App = () => {
+  // set up state for withdrawals
+  const [ withdrawals, setWithdrawals ] = useState([]);
+  const [data, setData] = useState([]);
+
+  const { response, loading, error } = useAxios({
+    method: 'placeholder'
+    url: "placeholder"
+    headers: 'placeholder'
+    body: JSON.stringify({
+      x
+      y
+      z
+    })
+  })
+
+  useEffect(() => {
+    if (response !== null) {
+      setData(response);
+    }
+  }, [response]);
+
+  setWithdrawals({...withdrawals, data})
+
+  // need a map function
+  const info = withdrawals.map((item) =>
+    <div>
+      <h2>
+      {key}
+      {amount}
+      {date}
+      </h2>
     </div>
-  );
+  )
+  return (
+    <div>
+      <h1>Zkopru withdrawals</h1>
+      {info}
+    </div>
+  )
 }
-
 export default App;
