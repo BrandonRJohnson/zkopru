562aa2986c74bd88791913d08fc85d9c3f8daa00
Brandon Johnson <brj.webside@gmail.com>
2021-09-01 16:42:07 -0500

    feat: Return raw expiration data and converted status data from API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
-import sampleData from './sampleData.json';
+import axios from 'axios';
  
 const App = () => {
+  // we need to use the api key in order to figure out the data that is actually being imported from the API
+  const [ info, setInfo ] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://zkopru.goerli.rollupscan.io/instant-withdraw', {mode: 'cors'})
+      .then((response) => {
+        console.log(response);
+        setInfo(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    }, [])
+  // Convert status from number in data to display words in browser (4 is available, 5 Not Available, 6 Fulfilled )
+  const statusData = info.map((item, index) => {
+    if ( info[index].withdrawal.status === 4 ) {
+      return (
+        <tr className="status" >
+          <td>Available</td>
+        </tr>
+      )
+    }
+
+    else if ( info[index].withdrawal.status === 6 ) {
+      return (
+        <tr>Fulfilled</tr>
+      )
+    }
+
+    else {
+      return (
+        <tr>Not Available</tr>
+      )
+    }
+  })
+
+  // Reward: Should be calculated based on withdrawal.eth - prepayFeeInEther
+  // should use the BN math library in order to do these calculations
+
+
+  // expiration: unix time (seconds), convert to milliseconds to use in javascript (e.g. new Date(expiration * 1000)
+  const getDate = (index) = {
+    let expiration = new Date(info[index].expiration * 1000)
+  } 
+  const expirationData = info.map((item, index) => {
+    return (
+      <tr>
+        <td>{expiration}</td>
+      </tr>
+    )
+  })
+
   return (
     <div>
       <h1>
         Market
       </h1>
+      <h2>
+      </h2>
       <table className="content-table">
         <thead>
           <tr>
-            <th>Status</th>
+            <th className="status">Status</th>
             <th>Reward</th>                  
             <th>Paid</th>
             <th>Expires</th>
@@ -23,21 +78,8 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          {sampleData.map((item,index) => {
-            return(
-              <div>
-                <td>{item.status}</td>
-                <td>{item.reward}</td>
-                <td>{item.paid}</td>
-                <td>{item.expirationTime}</td>
-                <td></td>
-                <td></td>
-                <td>{item.creationTime}</td>
-                <td>{item.asset}</td>
-                <td>{item.amount}</td>
-              </div>
-            )
-          })}
+          <tc> {statusData} </tc>
+          <tc> {expirationData} </tc>
         </tbody>
       </table>
     </div>
