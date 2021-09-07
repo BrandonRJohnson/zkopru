d7af7ff52504e047424f3e0065c1b44cd4e752ba
Brandon Johnson <brj.webside@gmail.com>
2021-09-07 10:53:49 -0500

    reafactor: Made map statement and status conditionals more concise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
 import axios from 'axios';
-import BN from 'bn.js'
  
 const App = () => {
   // we need to use the api key in order to figure out the data that is actually being imported from the API
   const [ info, setInfo ] = useState([])
+  const [ status, setStatus] = useState([])
 
   useEffect(() => {
     axios
@@ -18,71 +18,44 @@ const App = () => {
         console.log(error)
       })
     }, [])
-    
   // Status: convert status from number in data to display words in browser (4 is available, 5 Not Available, 6 Fulfilled )
   const Data = info.map((item, index) => {
-    if ( info[index].withdrawal.status === 4 ) {
-      return (
-        <tbody>
-          <tr>
-            <td>Available</td>
-            <td>{(info[index].prepayFeeInEth / (10 ** 18))} ETH</td>
-            <td>TBD</td>
-            <td>{ Date(info[index].expiration * 1000)}</td>
-            <td>TODO</td>
-            <td>TODO</td>
-            <td>TODO</td>
-            <td>ETH</td>
-          </tr>
-        </tbody>
-      )
-    }
-
-    else if ( info[index].withdrawal.status === 6 ) {
-      return (
-        <tbody>
-          <tr>
-            <td>Fulfilled</td>
-          </tr>
-          <tr>
-            <td>{(info[index].prepayFeeInEth / (10 ** 18))} ETH</td>
-          </tr>
-          <tr>
-            <td>{ Date(info[index].expiration * 1000)}</td>
-          </tr>
-        </tbody>
-      )
-    }
-
-    else {
-      return (
-        <tbody>
-          <tr>
-            <td>Not Available</td>
-          </tr>
-          <tr>
-            <td>{(info[index].prepayFeeInEth / (10 ** 18))} ETH</td>
-          </tr>
-          <tr>
-            <td>{ Date(info[index].expiration * 1000)}</td>
-          </tr>
-        </tbody>
-      )
-    }
+    return (
+      <tbody>
+        <tr>
+          <td>
+            {() => {
+              switch (status) {
+                case info[index].withdrawal.status === 4: return setStatus(status => [...status,'Available']);
+                case info[index].withdrawal.status === 5: return setStatus(status => [...status,'Fulfilled']);
+                default: return 'Not Available';
+              }
+            }}
+          </td>
+          <td>{(info[index].prepayFeeInEth / (10 ** 18))} ETH</td>
+          <td>TBD</td>
+          <td>{ Date(info[index].expiration * 1000)}</td>
+          <td>TODO</td>
+          <td>TODO</td>
+          <td>TODO</td>
+          <td>ETH</td>
+        </tr>
+      </tbody>
+    )
   })
+
 
   // Reward: Should be calculated based on withdrawal.eth - prepayFeeInEther
   // should use the BN math library in order to do these calculations
   // Paid:
 
-  // Expiration: unix time (seconds), convert to milliseconds to use in javascript (e.g. new Date(expiration * 1000)
+  // Expiration: unix time (seconds) convert to milliseconds to use in javascript (e.g. new Date(expiration * 1000)
 
   // Creation
 
   // Asset
 
   // Amount
-
   return (
     <div>
       <h1>
@@ -108,6 +81,7 @@ const App = () => {
       </table>
     </div>
   )
+
 }
 
 export default App;
