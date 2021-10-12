a1473c75c79922367c9f02360544c3bbb0058f13
Brandon Johnson <brj.webside@gmail.com>
2021-10-11 19:53:39 -0500

    Feat: Connected metamask to the website

--- src/App.js
+++ src/App.js
@@ -5,21 +5,41 @@ import fromWei from './fromWei.js';
 import newDate from './dayjs.js';
 import Alarmclock_gray from './Alarmclock_gray.svg'
 import Alarmclock_orange from './Alarmclock_orange.svg'
-import Alarmclock_yellow from './Alarmclock_yellow.svg'
 import Reward from "./Reward";
 
 const App = () => {
   // we need to use the api key in order to figure out the data that is actually being imported from the API
   const [ info, setInfo ] = useState([])
 
-  useEffect(async() => {
-    try {
+  useEffect(() => {
+    new Promise((resolve, reject) => {
+      window.addEventListener("load", async () => {
+        if (window.ethereum) {
+          const web3 = window.ethereum
+          try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' })
+            resolve(web3);
+          } catch (error) {
+            reject(error)
+          }
+        }
+        else if (window.web3) {
+          const web3 = window.web3
+          console.log("Injected web3 detected")
+          resolve(web3);
+        }
+        else {
+          console.log('Error, no web3 found in browser')
+        }
+      })
+    })
+
+    async function fetchData() {
       const { data } = await axios('https://zkopru.goerli.rollupscan.io/instant-withdraw')
       setInfo(data)
-      console.log(data)
-    } catch (err) {
-      console.log(err)
     }
+
+    fetchData()
   }, [])
 
   console.log(info)
@@ -28,7 +48,6 @@ const App = () => {
     if ( +address === 0) {
         return "ETH"
     }
-
     else {
         return tokenInfo.symbol
     }
@@ -47,51 +66,76 @@ const App = () => {
     return "Available"
   }
 
-  const Data = info.map((item) => {
-    const currentStatus = getStatus(item)
-    const isAvailable = currentStatus === "Available"
-    const isExpired = +new Date() > +item.expiration * 1000
-    const isWarning = +new Date() > +(item.expiration * 1000) - 3600
-      return (
-        <div className='table-row' style = {{backgroundColor: isAvailable ? "#EFF6FF" : "white", color: isExpired ? "#9ca3af" : "black" }} >
-            <div className="table-data-available" style={{ width: "10%" }}> {currentStatus} </div>
-            <div className="table-data" style={{ width: "15%"}}> {Reward(item)}</div>
-            <div className="table-data" style={{ width: "12%"}}>TBD</div>
-            <div className="table-data" style={{ width: "14%", display: 'flex', alignItems: 'center'}}>
-              <img src={isExpired ? Alarmclock_gray : isWarning ? Alarmclock_yellow : Alarmclock_orange } alt="" width= "20"></img>
-              {newDate(item.expiration)}
-            </div>
-            <div className="table-data" style={{ width: "8%"}}>{item.withdrawal.proposal.canonicalNum}</div>
-            <div className="table-data" style={{ width: "10%"}}>{item.withdrawal.proposal.proposedAt}</div>
-            <div className="table-data" style={{ width: "12%"}}>{newDate(item.withdrawal.proposal.timestamp)}</div>
-            <div className="table-data" style={{ width: "8%"}}>{getAssetSymbol(item.withdrawal.tokenAddr, item.withdrawal.tokenInfo)}</div>
-            <div className="table-data" style={{ width: "15%"}}>{fromWei(item.prepayFeeInEth)} {getAssetSymbol(item.withdrawal.tokenAddr, item.withdrawal.tokenInfo)} </div>
+  const filterExpired = info.filter(function(item) {
+    return +new Date() > +item.expiration * 1000
+  });
+
+  const filterAvailable = info.filter(function(item) {
+    return getStatus(item) === "Available"
+  });
+
+  // map function formatting the available
+
+  const availableWithdrawals = filterAvailable.map((item) => {
+    return (
+      <div className='table-row'>
+        <div className="table-data" style={{ width: "15%"}}> {Reward(item)}</div>
+        <div className="table-data" style={{ width: "14%", display: 'flex', alignItems: 'center'}}>
+        <img src={Alarmclock_orange} alt="" width= "20"></img>
+        {newDate(item.expiration)}
         </div>
-      )
+        <div className="table-data" style={{ width: "14%"}}> Loan </div>
+        <div className="table-data" style={{ width: "15%"}}>{fromWei(item.prepayFeeInEth)} {getAssetSymbol(item.withdrawal.tokenAddr, item.withdrawal.tokenInfo)} </div>
+        <div className="table-data" style={{ width: "8%"}}>{getAssetSymbol(item.withdrawal.tokenAddr, item.withdrawal.tokenInfo)}</div>
+        <div className="table-data" style={{ width: "12%"}}>{newDate(item.withdrawal.proposal.timestamp)}</div>
+        <div className="table-data" style={{ width: "8%"}}>{item.withdrawal.proposal.canonicalNum}</div>
+        <div className="table-data" style={{ width: "10%"}}>{item.withdrawal.proposal.proposedAt}</div>
+      </div>
+    )
+  })
+
+  // map function formatting the expired
+
+  const expiredWithdrawals = filterExpired.map((item) => {
+    return (
+      <div className='table-row-expired'>
+        <div className="table-data-expired" style={{ width: "15%"}}> {Reward(item)}</div>
+        <div className="table-data-expired" style={{ width: "14%", display: 'flex', alignItems: 'center'}}>
+          <img src={Alarmclock_gray} alt="" width= "20"></img>
+          {newDate(item.expiration)}
+        </div>
+        <div className="table-data-expired" style={{ width: "15%"}}>{fromWei(item.prepayFeeInEth)} {getAssetSymbol(item.withdrawal.tokenAddr, item.withdrawal.tokenInfo)} </div>
+        <div className="table-data-expired" style={{ width: "8%"}}>{getAssetSymbol(item.withdrawal.tokenAddr, item.withdrawal.tokenInfo)}</div>
+        <div className="table-data-expired" style={{ width: "12%"}}>{newDate(item.withdrawal.proposal.timestamp)}</div>
+        <div className="table-data-expired" style={{ width: "8%"}}>{item.withdrawal.proposal.canonicalNum}</div>
+        <div className="table-data-expired" style={{ width: "10%"}}>{item.withdrawal.proposal.proposedAt}</div>
+      </div>
+    )
   })
 
   return (
     <div>
       <h1 className="title">
-        Market
+         Prepay Market
       </h1>
-      <div className="table-container">
-        <div className="table-header">
-          <div style={{ width: "10%" }}>Status</div>
-          <div style={{ width: "15%" }}>Reward</div>
-          <div style={{ width: "12%" }}>Paid</div>
-          <div style={{ width: "14%", display: 'flex', alignItems: 'center'}}>
-            <img src={Alarmclock_gray} width= "20"></img>
-            <div>Expires</div>
-          </div>
-          <div style={{ width: "8%" }}>L2 Block</div>
-          <div style={{ width: "10%" }}>L1</div>
-          <div style={{ width: "12%" }}>Creation</div>
-          <div style={{ width: "8%" }}>Asset</div>
-          <div style={{ width: "15%" }}>Amount</div>
-        </div>
-        {Data}
+      <br></br>
+      <h1 className="title">
+        Available withdrawals
+      </h1>
+      <div>
+        {availableWithdrawals}
       </div>
+      <br></br>
+      <h1 className="title">
+        Completed withdrawals
+      </h1>
+      <div>
+        {expiredWithdrawals}
+      </div>
+      <br></br>
+      <h1 className="title">
+        My Loans
+      </h1>
     </div>
   )
 
